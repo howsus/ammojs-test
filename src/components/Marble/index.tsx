641c8b160a49758e4e1d05b9498c8e894c991e1f
Ramiz Altynbaev <ramiz@quinncia.io>
2021-02-24 18:46:28 +0300

    Movement based on camera position

--- src/components/Marble/index.tsx
+++ src/components/Marble/index.tsx
@@ -24,10 +24,13 @@ const Marble: React.FC<MarbleProps> = ({ ...props }: MarbleProps) => {
     backward: 0,
     left: 0,
     right: 0,
+    upward: 0,
   };
 
   const handleKey = (event: KeyboardEvent, action = 0) => {
     const { keyCode } = event;
+
+    console.log('keyCode', keyCode, action);
 
     switch (keyCode) {
       case 87: // W: FORWARD
@@ -42,9 +45,14 @@ const Marble: React.FC<MarbleProps> = ({ ...props }: MarbleProps) => {
       case 68: // D: RIGHT
         moveDirection.right = action;
         break;
+      case 32: // SPACE: UPWARD
+        moveDirection.upward = action;
+        break;
       default:
         break;
     }
+
+    console.log('Object.values(moveDirection)', Object.values(moveDirection));
 
     if (Math.max(...Object.values(moveDirection)) === 1) {
       moveBall();
@@ -52,12 +60,12 @@ const Marble: React.FC<MarbleProps> = ({ ...props }: MarbleProps) => {
   };
 
   const handleKeyDown = useCallback(
-    (event: KeyboardEvent) => handleKey(event, 0),
+    (event: KeyboardEvent) => handleKey(event, 1),
     [],
   );
 
   const handleKeyUp = useCallback(
-    (event: KeyboardEvent) => handleKey(event, 1),
+    (event: KeyboardEvent) => handleKey(event, 0),
     [],
   );
 
@@ -73,27 +81,112 @@ const Marble: React.FC<MarbleProps> = ({ ...props }: MarbleProps) => {
   useEffect(() => position.subscribe((value) => {
     camera.lookAt(value[0], value[1], value[2]);
     camera.position.set(value[0] - 5, value[1] + 10, value[2] - 5);
+    // camera.position.set(value[0] + 5, value[1] + 10, value[2] + 5);
   }), []);
 
   const moveBall = () => {
+    let cameraDirectionVector = [
+      0,
+      0,
+    ];
+
+    if (ref && ref.current) {
+      console.log('ref', ref.current.position);
+
+      cameraDirectionVector = [
+        ref.current.position.x - camera.position.x,
+        ref.current.position.z - camera.position.z,
+      ];
+
+      const normFactor = Math.max(
+        Math.abs(cameraDirectionVector[0]),
+        Math.abs(cameraDirectionVector[1]),
+      );
+
+      cameraDirectionVector = [
+        cameraDirectionVector[0] / normFactor,
+        cameraDirectionVector[1] / normFactor,
+      ];
+
+      console.log('cameraDirectionVector', cameraDirectionVector);
+    }
+
     const scalingFactor = 1;
     const moveX = moveDirection.right - moveDirection.left;
     const moveZ = moveDirection.backward - moveDirection.forward;
+    const moveY = moveDirection.upward;
 
-    if (moveX === 0 && moveZ === 0) {
-      // return;
+    if (moveX === 0 && moveY === 0 && moveZ === 0) {
+      return;
     }
 
-    // position.set(10, 0, 0);
+    let movementDirectionVector = [0, 0];
 
-    // console.log('moveX', linearVelocity.get());
+    let mtx = [
+      [0, 0],
+      [0, 0],
+    ];
 
-    // const resultantImpulse = new Ammo.btVector3(moveX, moveY, moveZ);
-    // resultantImpulse.op_mul(scalingFactor);
-    // let physicsBody = ballObject.userData.physicsBody;
-    // physicsBody.setLinearVelocity( resultantImpulse );
-    // linearVelocity
-    linearVelocity.set(moveX * 20, 0, moveZ * 20);
+    if (moveX > 0) { // D
+      const D = [
+        [-1, 0],
+        [0, 1],
+      ];
+
+      mtx = [
+        [mtx[0][0] + D[0][0], mtx[1][0] + D[1][0]],
+        [mtx[1][0] + D[1][0], mtx[1][1] + D[1][1]],
+      ];
+    }
+
+    if (moveX < 0) { // A
+      const W = [
+        [1, 0],
+        [0, -1],
+      ];
+
+      mtx = [
+        [mtx[0][0] + W[0][0], mtx[1][0] + W[1][0]],
+        [mtx[1][0] + W[1][0], mtx[1][1] + W[1][1]],
+      ];
+    }
+
+    if (moveZ > 0) { // S
+      const W = [
+        [-1, 0],
+        [0, -1],
+      ];
+
+      mtx = [
+        [mtx[0][0] + W[0][0], mtx[1][0] + W[1][0]],
+        [mtx[1][0] + W[1][0], mtx[1][1] + W[1][1]],
+      ];
+    }
+
+    if (moveZ < 0) { // W
+      const W = [
+        [1, 0],
+        [0, 1],
+      ];
+
+      mtx = [
+        [mtx[0][0] + W[0][0], mtx[1][0] + W[1][0]],
+        [mtx[1][0] + W[1][0], mtx[1][1] + W[1][1]],
+      ];
+    }
+
+    if (mtx !== null) {
+      movementDirectionVector = [
+        mtx[0][0] * cameraDirectionVector[0] + mtx[0][1] * cameraDirectionVector[1],
+        mtx[1][0] * cameraDirectionVector[0] + mtx[1][1] * cameraDirectionVector[1],
+      ];
+    }
+
+    linearVelocity.set(
+      movementDirectionVector[0] * 1.4,
+      moveY * 10,
+      movementDirectionVector[1] * 1.4,
+    );
   };
 
   return (
